Bug Fixes in mAP calculation, add bias to architecture and change data augmentations.

data/box_utils.py
@@ -2,6 +2,15 @@ import torch
 import itertools
 from torchvision.ops import box_iou, box_convert
 
+def log_sum_exp(x):
+    """Utility function for computing log_sum_exp while determining
+    This will be used to determine unaveraged confidence loss across
+    all examples in a batch.
+    Args:
+        x (Variable(tensor)): conf_preds from conf layers
+    """
+    x_max = x.data.max()
+    return torch.log(torch.sum(torch.exp(x-x_max), 1, keepdim=True)) + x_max
 
 class AnchorBox(object):
     def __init__(self, s_min=0.2, s_max=0.95, img_size=300,
@@ -23,8 +32,8 @@ class AnchorBox(object):
             s_next = sk + next_add
             for i, j in itertools.product(range(f_size), range(f_size)):
                 fk = img_size/step
-                cx = (i + 0.5)/fk
-                cy = (j + 0.5)/fk
+                cx = (j + 0.5)/fk
+                cy = (i + 0.5)/fk
                 bxs += [[cx, cy, sk, sk]]
                 if num_bbxs == 3:
                     ratio = self.ratio[1:-1]
@@ -36,11 +45,51 @@ class AnchorBox(object):
                 if num_bbxs == 6:
                     bxs += [[cx, cy, (sk*s_next)**0.5, (sk*s_next)**0.5]]
             sk = s_next
-        self.anchor_box = torch.Tensor(bxs)
+        self.anchor_box = torch.Tensor(bxs).clamp_(min = 0,max = 1)
 
     def get_anchor(self):
-        return self.anchor_box
+        return self.anchor_box.clamp_(max=1,min = 0)
 
+
+def encode(matched, priors, variances=[0.1,0.2]):
+    """Encode the variances from the priorbox layers into the ground truth boxes
+    we have matched (based on jaccard overlap) with the prior boxes.
+    Args:
+        matched: (tensor) Coords of ground truth for each prior in point-form
+            Shape: [num_priors, 4].
+        priors: (tensor) Prior boxes in center-offset form
+            Shape: [num_priors,4].
+        variances: (list[float]) Variances of priorboxes
+    Return:
+        encoded boxes (tensor), Shape: [num_priors, 4]
+    """
+
+    # dist b/t match center and prior's center
+    g_cxcy = matched[:,:2] - priors[:, :2]
+    # encode variance
+    g_cxcy /= (variances[0] * priors[:, 2:])
+    # match wh / prior wh
+    g_wh = (matched[:, 2:]) / priors[:, 2:]
+    g_wh = torch.log(g_wh + 1e-10) / variances[1]
+    # return target for smooth_l1_loss
+    return torch.cat([g_cxcy, g_wh], 1) 
+
+def decode_new(loc, priors, variances=[0.1,0.2]):
+    """Decode locations from predictions using priors to undo
+    the encoding we did for offset regression at train time.
+    Args:
+        loc (tensor): location predictions for loc layers,
+            Shape: [num_priors,4]
+        priors (tensor): Prior boxes in center-offset form.
+            Shape: [num_priors,4].
+        variances: (list[float]) Variances of priorboxes
+    Return:
+        decoded bounding box predictions
+    """
+    boxes = torch.cat((
+        priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:],
+        priors[:, 2:] * torch.exp(loc[:, 2:] * variances[1])), 1)
+    return boxes
 
 def encode_coordinate(default_boxes, match_boxes):
     # bbxs: Num_anchorsx4 tensor with format (x,y,w,h)
@@ -55,14 +104,54 @@ def decode_coordinate(default_boxes, match_boxes):
     wh = (torch.exp(match_boxes[:, 2:])*default_boxes[:, 2:])
     return torch.cat([xy, wh], dim=1)
 
+def intersect(box_a, box_b):
+    """ We resize both tensors to [A,B,2] without new malloc:
+    [A,2] -> [A,1,2] -> [A,B,2]
+    [B,2] -> [1,B,2] -> [A,B,2]
+    Then we compute the area of intersect between box_a and box_b.
+    Args:
+      box_a: (tensor) bounding boxes, Shape: [A,4].
+      box_b: (tensor) bounding boxes, Shape: [B,4].
+    Return:
+      (tensor) intersection area, Shape: [A,B].
+    """
+    A = box_a.size(0)
+    B = box_b.size(0)
+    max_xy = torch.min(box_a[:, 2:].unsqueeze(1).expand(A, B, 2),
+                       box_b[:, 2:].unsqueeze(0).expand(A, B, 2))
+    min_xy = torch.max(box_a[:, :2].unsqueeze(1).expand(A, B, 2),
+                       box_b[:, :2].unsqueeze(0).expand(A, B, 2))
+    inter = torch.clamp((max_xy - min_xy), min=0)
+    return inter[:, :, 0] * inter[:, :, 1]
+
+
+def jaccard(box_a, box_b):
+    """Compute the jaccard overlap of two sets of boxes.  The jaccard overlap
+    is simply the intersection over union of two boxes.  Here we operate on
+    ground truth boxes and default boxes.
+    E.g.:
+        A ∩ B / A ∪ B = A ∩ B / (area(A) + area(B) - A ∩ B)
+    Args:
+        box_a: (tensor) Ground truth bounding boxes, Shape: [num_objects,4]
+        box_b: (tensor) Prior boxes from priorbox layers, Shape: [num_priors,4]
+    Return:
+        jaccard overlap: (tensor) Shape: [box_a.size(0), box_b.size(0)]
+    """
+    inter = intersect(box_a, box_b)
+    area_a = ((box_a[:, 2]-box_a[:, 0]) *
+              (box_a[:, 3]-box_a[:, 1])).unsqueeze(1).expand_as(inter)  # [A,B]
+    area_b = ((box_b[:, 2]-box_b[:, 0]) *
+              (box_b[:, 3]-box_b[:, 1])).unsqueeze(0).expand_as(inter)  # [A,B]
+    union = area_a + area_b - inter
+    return inter / union  # [A,B]
 
 def match_one_img(anchor, one_img_bbxs, one_img_label, result_tensor_box, result_tensor_class, index, input_format='cxcywh'):
     # the input must be [x,y,w,h]
     anchor_temp = box_convert(
         anchor, in_fmt=input_format, out_fmt='xyxy').clamp_(min=0, max=1)
-    one_img_bbxs = box_convert(
+    one_img_bbx = box_convert(
         one_img_bbxs, in_fmt=input_format, out_fmt='xyxy').clamp_(min=0, max=1)
-    IoUs = box_iou(anchor_temp, one_img_bbxs)  # num_anchors x num_bbxs
+    IoUs = box_iou(anchor_temp, one_img_bbx)  # num_anchors x num_bbxs
     #one_img_cls, one_img_offset = [], []
     idx1 = torch.argmax(IoUs, dim=0)  # idx cua defaul_bxs
     # asure every groundtruth has a matching default box
@@ -76,7 +165,7 @@ def match_one_img(anchor, one_img_bbxs, one_img_label, result_tensor_box, result
     result_tensor_class[index] = (truth_lab_temp[
         range(anchor_temp.shape[0]), idx2]).to(torch.int64)  # ndef
     matched_bbxs = truth_bx_temp[range(anchor_temp.shape[0]), idx2]
-    result_tensor_box[index] = encode_coordinate(anchor_temp, matched_bbxs)
+    result_tensor_box[index] = encode(matched_bbxs,anchor)
     one_img_cls_idx = IoUbest <= 0.5
     one_img_cls_idx[idx1] = False
     result_tensor_class[index][one_img_cls_idx] = 0
@@ -84,6 +173,63 @@ def match_one_img(anchor, one_img_bbxs, one_img_label, result_tensor_box, result
 
     # output: num_anchors x 4: regressor label, num_anchors: class_label
 
+def point_form(boxes):
+    """ Convert prior_boxes to (xmin, ymin, xmax, ymax)
+    representation for comparison to point form ground truth data.
+    Args:
+        boxes: (tensor) center-size default boxes from priorbox layers.
+    Return:
+        boxes: (tensor) Converted xmin, ymin, xmax, ymax form of boxes.
+    """
+    return torch.cat((boxes[:, :2] - boxes[:, 2:]/2,     # xmin, ymin
+                     boxes[:, :2] + boxes[:, 2:]/2), 1)  # xmax, ymax
+
+def match(threshold, truths, priors, labels, loc_t, conf_t, idx,input_format = 'cxcywh'):
+    """Match each prior box with the ground truth box of the highest jaccard
+    overlap, encode the bounding boxes, then return the matched indices
+    corresponding to both confidence and location preds.
+    Args:
+        threshold: (float) The overlap threshold used when mathing boxes.
+        truths: (tensor) Ground truth boxes, Shape: [num_obj, num_priors].
+        priors: (tensor) Prior boxes from priorbox layers, Shape: [n_priors,4].
+        variances: (tensor) Variances corresponding to each prior coord,
+            Shape: [num_priors, 4].
+        labels: (tensor) All the class labels for the image, Shape: [num_obj].
+        loc_t: (tensor) Tensor to be filled w/ endcoded location targets.
+        conf_t: (tensor) Tensor to be filled w/ matched indices for conf preds.
+        idx: (int) current batch index
+    Return:
+        The matched indices corresponding to 1)location and 2)confidence preds.
+    """
+    # jaccard index
+    priors = box_convert(
+        priors, in_fmt=input_format, out_fmt='xyxy').clamp_(min=0, max=1)
+    truths = box_convert(
+        truths, in_fmt=input_format, out_fmt='xyxy').clamp_(min=0, max=1)
+    overlaps = jaccard(
+        point_form(truths),
+        point_form(priors)
+    )
+    # (Bipartite Matching)
+    # [1,num_objects] best prior for each ground truth
+    best_prior_overlap, best_prior_idx = overlaps.max(1, keepdim=True)
+    # [1,num_priors] best ground truth for each prior
+    best_truth_overlap, best_truth_idx = overlaps.max(0, keepdim=True)
+    best_truth_idx.squeeze_(0)
+    best_truth_overlap.squeeze_(0)
+    best_prior_idx.squeeze_(1)
+    best_prior_overlap.squeeze_(1)
+    best_truth_overlap.index_fill_(0, best_prior_idx, 2)  # ensure best prior
+    # TODO refactor: index  best_prior_idx with long tensor
+    # ensure every gt matches with its prior of max overlap
+    for j in range(best_prior_idx.size(0)):
+        best_truth_idx[best_prior_idx[j]] = j
+    matches = truths[best_truth_idx]          # Shape: [num_priors,4]
+    conf = labels[best_truth_idx]          # Shape: [num_priors]
+    conf[best_truth_overlap <= threshold] = 0  # label as background
+    loc = encode(matches,priors)
+    loc_t[idx] = loc    # [num_priors,4] encoded offsets to learn
+    conf_t[idx] = conf  # [num_priors] top class label for each prior
 
 def match_batch(anchor, img_bbxs, img_labels, input_format='cxcywh', device='cuda:0'):
     assert len(img_bbxs) == len(img_labels)
@@ -91,55 +237,76 @@ def match_batch(anchor, img_bbxs, img_labels, input_format='cxcywh', device='cud
     result_bbxs = torch.zeros((batch, anchor.shape[0], 4))
     result_labels = torch.zeros((batch, anchor.shape[0]), dtype=torch.int64)
     for i in range(len(img_bbxs)):
-        match_one_img(anchor, img_bbxs[i], img_labels[i],
-                      result_bbxs, result_labels, i, input_format)
-    ret = {"bbxs": result_bbxs.to(device), "clss": result_labels.to(device)}
+        # match(0.5,img_bbxs[i],anchor, img_labels[i],
+        #               result_bbxs, result_labels, i,input_format)
+        match_one_img(anchor, img_bbxs[i], img_labels[i], result_bbxs, result_labels, i, input_format='cxcywh')
+    ret = {"bbxs": result_bbxs.to(device), "clss": result_labels.to(device),'anchor':anchor}
     return ret
 
+def IoG(box_a, box_b):
+    """Compute the IoG of two sets of boxes.  
+    E.g.:
+        A ∩ B / A = A ∩ B / area(A)
+    Args:
+        box_a: (tensor) Ground truth bounding boxes, Shape: [num_objects,4]
+        box_b: (tensor) Prior boxes from priorbox layers, Shape: [num_objects,4]
+    Return:
+        IoG: (tensor) Shape: [num_objects]
+    """
+    inter_xmin = torch.max(box_a[:, 0], box_b[:, 0])
+    inter_ymin = torch.max(box_a[:, 1], box_b[:, 1])
+    inter_xmax = torch.min(box_a[:, 2], box_b[:, 2])
+    inter_ymax = torch.min(box_a[:, 3], box_b[:, 3])
+    Iw = torch.clamp(inter_xmax - inter_xmin, min=0)
+    Ih = torch.clamp(inter_ymax - inter_ymin, min=0)  
+    I = Iw * Ih
+    G = (box_a[:, 2] - box_a[:, 0]) * (box_a[:, 3] - box_a[:, 1])
+    return I / G
 
 if __name__ == "__main__":
-    pass
-    # import pandas as pd
-    # from dataprep import ImageData
-    # import torchvision
-    # import numpy as np
-    # import PIL.Image as Image
-    # from torch_snippets import *
-    # ROOT = '../fastRCNNdata'
-    # dataset = {
-    #     'imgs_path': ROOT+'/images/images',
-    #     'annotations': ROOT + '/df.csv',
-    #     'normalize': {'mean': [0.485, 0.456, 0.406], 'std': [0.229, 0.224, 0.225]},
-    #     'candidate_boxes_path': ROOT + '/images/candidates',
-    #     'candidate_boxes_class': ROOT + '/images/classes',
-    #     'candidate_boxes_delta': ROOT + '/images/delta',
-    #     'num_workers': 8,
-    #     'IoU_threshold': 0.35,
-    #     'train_ratio': 0.7,
-    #     'test_ratio': 0.15,
-    #     'weight_decay': 0.0005,
-    # }
-    # a = AnchorBox()
-    # default_bxs = a.get_anchor()
-    # df_raw = pd.read_csv(dataset['annotations'])
-    # test_ds = ImageData(df_raw, df_raw, dataset['imgs_path'], phase='test')
-    # img_tensor1, a1, b1, img_path1 = test_ds[12]
-    # img_tensor2, a2, b2, img_path2 = test_ds[128]
-    # b_im = torch.stack([img_tensor1, img_tensor2])
-    # a = [a1, a2]
-    # b = [b1, b2]
-    # result = match_batch(default_bxs, a, b)
+    #pass
+    import pandas as pd
+    from dataprep import ImageData
+    import torchvision
+    import numpy as np
+    import PIL.Image as Image
+    from torch_snippets import *
+    ROOT = '../my_data'
+    dataset = {
+        'imgs_path': ROOT+'/images/images',
+        'annotations': ROOT + '/df.csv',
+        'normalize': {'mean': [0.485, 0.456, 0.406], 'std': [0.229, 0.224, 0.225]},
+        'candidate_boxes_path': ROOT + '/images/candidates',
+        'candidate_boxes_class': ROOT + '/images/classes',
+        'candidate_boxes_delta': ROOT + '/images/delta',
+        'num_workers': 8,
+        'IoU_threshold': 0.35,
+        'train_ratio': 0.7,
+        'test_ratio': 0.15,
+        'weight_decay': 0.0005,
+    }
+    a = AnchorBox()
+    default_bxs = a.get_anchor()
+    df_raw = pd.read_csv(dataset['annotations'])
+    test_ds = ImageData(df_raw, df_raw, dataset['imgs_path'], phase='train')
+    img_tensor1, a1, b1, img_path1 = test_ds[1]
+    img_tensor2, a2, b2, img_path2 = test_ds[128]
+    b_im = torch.stack([img_tensor1, img_tensor2])
+    a = [a1, a2]
+    b = [b1, b2]
+    result = match_batch(default_bxs, a, b)
 
-    # deltas = result['bbxs'].squeeze()[1]
-    # label = result['clss'].squeeze()[1]
-    # idx = torch.where(label != 0)[0]
-    # match_bbxs = decode_coordinate(default_bxs, deltas)
-    # match_bbxs = torchvision.ops.box_convert(
-    #     match_bbxs[idx], in_fmt='cxcywh', out_fmt='xyxy')
-    # match_bbxs = match_bbxs
-    # label = label[idx]
-    # h, w, _ = np.array(Image.open(img_path2)).shape
-    # fin = match_bbxs.clamp_(0, 1)*torch.tensor([w, h, w, h])
-    # show(Image.open(img_path2), bbs=fin, texts=[
-    #      test_ds.idx2class[i.item()] for i in label])
-    # plt.savefig('test_img/test_match_box.png')
+    deltas = result['bbxs'].squeeze()[1]
+    label = result['clss'].squeeze()[1]
+    idx = torch.where(label != 0)[0]
+    match_bbxs = decode_new(deltas.cuda(),default_bxs.cuda(),)
+    match_bbxs = torchvision.ops.box_convert(
+        match_bbxs[idx], in_fmt='cxcywh', out_fmt='xyxy')
+    match_bbxs = match_bbxs
+    label = label[idx]
+    #h, w, _ = np.array(Image.open(img_path1)).shape
+    _,h, w = img_tensor2.shape
+    fin = match_bbxs.clamp_(0, 1)*torch.tensor([w, h, w, h]).cuda()
+    show(img_tensor2.permute(1, 2, 0), bbs=fin, texts=[
+         test_ds.idx2class[i.item()] for i in label])
+    plt.savefig('test_img/test_match_box.png')

data/dataprep.py
@@ -8,6 +8,7 @@ import numpy as np
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 import matplotlib
+import cv2
 matplotlib.use('svg')
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -23,14 +24,14 @@ class RandomExpand(object):
         if prop == 0:
             return img, bbxs
         else:
-            height, width, _ = img.shape
+            _ , height, width = img.shape
             ratio = np.random.uniform(1, 3)
             left = np.random.uniform(0, ratio*width - width)
             top = np.random.uniform(0, ratio*height-height)
             blanc_img = np.zeros(
-                shape=(int(ratio*height), int(ratio*width), 3), dtype=np.uint8)
-            blanc_img[:, :, :] = np.mean(img, axis=(0, 1))
-            blanc_img[int(top):int(top+height), int(left):int(left+width), :] = img
+                shape=(3,int(ratio*height), int(ratio*width)), dtype=np.float32)
+            blanc_img[:, :, :] = np.mean(img, axis=(1, 2),keepdims=True)
+            blanc_img[:,int(top):int(top+height), int(left):int(left+width)] = img
             bx = bbxs.clone()
             bx = bx*torch.tensor([width, height, width, height]) + \
                 torch.tensor([left, top, 0, 0])
@@ -49,7 +50,7 @@ class RandomFlip(object):
         if prop == 0:
             return img, bbxs
         else:
-            img = img[:, ::-1, :]
+            img = img[:, :, ::-1]
             bbxs[:, 0] = 1-bbxs[:, 0]
             return img, bbxs
 
@@ -66,8 +67,8 @@ class RandomCrop(object):
             return img, bbxs, labels
         else:
             success_flag = 0
-            height, width, _ = img.shape
-            min_ious = [0.1, 0.3, 0.7, 0.9]
+            _,height, width = img.shape
+            min_ious = [0.1, 0.3, 0.7, 0.9,-np.inf]
             i = 0
             min_iou = np.random.choice(min_ious)
             while success_flag == 0 and i < self.max_step:
@@ -85,7 +86,7 @@ class RandomCrop(object):
                 bx_tem = torchvision.ops.box_convert(
                     bbxs, 'cxcywh', 'xyxy').clamp(min=0, max=1)
                 IoUmat = torchvision.ops.box_iou(reg_tem, bx_tem)
-                if torch.amax(IoUmat) > min_iou:
+                if torch.amin(IoUmat)>=min_iou:
                     center_x = bbxs[:, 0]
                     center_y = bbxs[:, 1]
                     con_x = (reg_tem[0, 0] <= center_x) & (
@@ -119,37 +120,67 @@ class RandomCrop(object):
                         nw = (rw - lw)/w
                         nh = (bh - th)/h
                         cropped_bbxs = torch.stack([cx, cy, nw, nh], dim=1)
-                        return img[int(top):int(top+h), int(left):int(left+w), :], cropped_bbxs, tem_label
+                        return img[:,int(top):int(top+h), int(left):int(left+w)], cropped_bbxs, tem_label
                 i += 1
             if success_flag == 0:
                 return img, bbxs, labels
 
+def apply_transform_with_prop(img,transform,p):
+    #img: Tensor
+    #transform:f function/ callable class
+    #p: float32 properbility
+    prop = np.random.choice([0, 1], p=[1-p, p])
+    if prop:
+        img = transform(img)
+    return img
+
+
+
+def ligning_noise_trans(img):
+    perms = ((0, 1, 2), (0, 2, 1),
+                    (1, 0, 2), (1, 2, 0),
+                    (2, 0, 1), (2, 1, 0))
+
+    swap = perms[np.random.randint(len(perms))]
+    img = img[swap,:,:]
+    return img
+    
 
 def prep_process_img_train(img, bbxs, labels):
+    base_aug  = transforms.ToTensor()
+    brightness_aug = transforms.ColorJitter(0.125,0,0,0)
+    distort_aug = transforms.ColorJitter(0, 0, (0.5,1.5), 0.05)
+    constrast_aug = transforms.ColorJitter(0,(0.5,1.5),0,0)
+    distort_order = [transforms.Compose([constrast_aug,distort_aug]),transforms.Compose([distort_aug,constrast_aug])][np.random.choice([0, 1], p=[0.5, 0.5])]
+    ligning_noise = ligning_noise_trans
 
-    transf = transforms.Compose([transforms.ColorJitter(0.3, 0.2, 0.1, 0.1)])
+    random_crop = RandomCrop(0.5)
+    random_flip = RandomFlip(0.5)
+    random_expand = RandomExpand(0.5)
+    normalize_aug = transforms.Compose([transforms.Resize((300,300),antialias=True),transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                     std=[0.229, 0.224, 0.225])])
 
-    trans = transforms.Compose([transforms.Resize((300, 300)),
-                                transforms.ToTensor(),
-                                transforms.Normalize(mean=[0.5, 0.5, 0.5],
-                                                     std=[0.5, 0.5, 0.5])])
-    rd_ex = RandomExpand(0.5)
-    rd_crop = RandomCrop(0.5)
-    rd_flip = RandomFlip(0.5)
-    transed = np.array(transf(img))
-    #transed, bx = rd_ex(transed, bbxs)
-    transed, bx, ret_labels = rd_crop(transed, bbxs, labels)
-    transed, bx = rd_flip(transed, bx)
+    #run_augment
+    img = base_aug(img)
+    img = apply_transform_with_prop(img,brightness_aug,0.5)
+    img = apply_transform_with_prop(img,distort_order,0.5)
+    img = apply_transform_with_prop(img,ligning_noise,0.5)
 
-    return trans(Image.fromarray(transed)), bx, ret_labels
+    img = img.numpy()
+
+    img, bx = random_expand(img, bbxs)
+    img, bx, ret_labels = random_crop(img, bx, labels)
+    img, bx = random_flip(img, bx)
+    img = torch.from_numpy(img.copy())
+
+    return normalize_aug(img),bx, ret_labels
 
 
 def prep_process_img_test(img):
-    trans = transforms.Compose([transforms.Resize((300, 300)),
-                                transforms.ToTensor(),
-                                transforms.Normalize(mean=[0.5, 0.5, 0.5],
-                                                     std=[0.5, 0.5, 0.5])])
-
+    trans = transforms.Compose([transforms.ToTensor(),
+                                transforms.Resize((300, 300),antialias=True),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                     std=[0.229, 0.224, 0.225])])
     return trans(img)
 
 
@@ -221,7 +252,7 @@ if __name__ == '__main__':
     pass
     import pandas as pd
     from torch_snippets import *
-    ROOT = '../fastRCNNdata'
+    ROOT = '../my_data'
     dataset = {
         'imgs_path': ROOT+'/images/images',
         'annotations': ROOT + '/df.csv',

loss/loss.py
@@ -4,24 +4,56 @@ import math
 import torch.nn.functional as F
 
 
+import torch
+import math
+import torch.nn.functional as F
+from torch.autograd import Variable
+from data.box_utils import log_sum_exp,decode_new,IoG
+import torch.nn as nn
+
+
+
+
 def multibox_loss(preds_labels, preds_bbxs, target_labels,
-                  target_bbxs, bx_loss=torch.nn.SmoothL1Loss(
-        reduction='sum'),
+                  target_bbxs, priors,bx_loss=torch.nn.SmoothL1Loss(
+        reduction='mean'),
         class_loss=torch.nn.CrossEntropyLoss(reduction='none')):
-    pos_mask = torch.where(target_labels != 0, 1, 0).unsqueeze(-1)
-    num_not_neg = torch.sum(pos_mask)
-    if num_not_neg == 0:
-        reg_loss = torch.tensor([0]).cuda()
-        cls_loss = torch.tensor([0]).cuda()
-    else:
-        reg_loss = bx_loss(
-            preds_bbxs*pos_mask, target_bbxs*pos_mask)/num_not_neg
-        cls_loss = class_loss(
-            preds_labels.permute(0, 2, 1), target_labels)  # Bxn_anchorxC -> BxCxn_anchors
-        pos_cls = (pos_mask.squeeze(-1)*cls_loss).sum()
-        neg_cls, _ = ((1-pos_mask.squeeze(-1)) *
-                      cls_loss).sort(descending=True, dim=-1)  # hard negative minings with (num_pos/num_negs)=1/3
-        num_neg = min(3*num_not_neg, len(neg_cls)-num_not_neg)
-        neg_cls = neg_cls[:, :num_neg]
-        cls_loss = (pos_cls + neg_cls.sum())/num_not_neg
-    return reg_loss + cls_loss, reg_loss.detach(), cls_loss.detach()
+        loc_t = Variable(target_bbxs, requires_grad=False)
+        conf_t = Variable(target_labels, requires_grad=False)
+        num = preds_bbxs.size(0)
+        pos = conf_t > 0
+        num_pos = pos.sum(dim=1, keepdim=True)
+
+        # Localization Loss (Smooth L1)
+        # Shape: [batch,num_priors,4]
+        pos_idx = pos.unsqueeze(pos.dim()).expand_as(preds_bbxs)
+        loc_p = preds_bbxs[pos_idx].view(-1, 4)
+        loc_t = loc_t[pos_idx].view(-1, 4)
+        loss_l = F.smooth_l1_loss(loc_p, loc_t, size_average=False)
+
+        # Compute max conf across batch for hard negative mining
+        batch_conf = preds_labels.view(-1, 3)
+        loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
+
+        # Hard Negative Mining
+        loss_c[pos.view(-1)] = 0  # filter out pos boxes for now
+        loss_c = loss_c.view(num, -1)
+        _, loss_idx = loss_c.sort(1, descending=True)
+        _, idx_rank = loss_idx.sort(1)
+        num_pos = pos.long().sum(1, keepdim=True)
+        num_neg = torch.clamp(3*num_pos, max=pos.size(1)-1)
+        neg = idx_rank < num_neg.expand_as(idx_rank)
+
+        # Confidence Loss Including Positive and Negative Examples
+        pos_idx = pos.unsqueeze(2).expand_as(preds_labels)
+        neg_idx = neg.unsqueeze(2).expand_as(preds_labels)
+        conf_p = preds_labels[(pos_idx+neg_idx).gt(0)].view(-1, 3)
+        targets_weighted = conf_t[(pos+neg).gt(0)]
+        loss_c = F.cross_entropy(conf_p, targets_weighted, size_average=False)
+
+        # Sum of losses: L(x,c,l,g) = (Lconf(x, c) + αLloc(x,l,g)) / N
+
+        N = num_pos.data.sum()
+        loss_l /= N
+        loss_c /= N
+        return loss_l+loss_c,loss_l, loss_c

loss/metric.py
@@ -4,82 +4,178 @@
 
 from torchvision.ops import box_iou, box_convert, nms
 import torch
+from collections import Counter
 # i get the idea from: https://www.youtube.com/watch?v=FppOzcDvaDI&ab_channel=AladdinPersson
+import numpy as np
 
 
-def FP_TP_one_img(preds_bbxs, preds_labels, truth_bbxs, truth_labels, nums_class, iou_threshold=0.5, device='cuda:0'):
-    # return dict {class for each class}
-    # input format must be xyxy
-    d = {}
-    gt_d = {}
-    max_preds, class_preds = preds_labels.max(dim=1)
-    for c in range(1, nums_class):  # 0 denote background
-        box_of_c = preds_bbxs[class_preds == c, :].clamp(min=0, max=1)
-        TP = torch.zeros(box_of_c.shape[0])
-        FP = torch.zeros(box_of_c.shape[0])
-        truth_of_c = truth_bbxs[truth_labels == c, :]
-        gt_d[c] = truth_of_c.shape[0]
-        if truth_of_c.shape[0] == 0:
-            if box_of_c.shape[0] == 0:
-                d[c] = []
-                continue
-            FP[:] = 1
-            d[c] = torch.stack([max_preds[class_preds == c],
-                               TP.to(device), FP.to(device)], dim=1)
-        else:
-            if box_of_c.shape[0] == 0:
-                d[c] = []
-                continue
-            iou = box_iou(box_of_c, truth_of_c)  # num_pred_c x num_truth_c
-            max_iou, max_iou_idx = iou.max(dim=0)
-            true_pt = max_iou >= iou_threshold
-            if len(true_pt) > 0:  # remove overlaps
-                TP[max_iou_idx[true_pt]] = 1
-            FP[TP == 0] = 1
-            d[c] = torch.stack([max_preds[class_preds == c],
-                               TP.to(device), FP.to(device)], dim=1)
-    return d, gt_d
 
+def voc_ap(rec, prec, use_07_metric=False):
+    """ ap = voc_ap(rec, prec, [use_07_metric])
+    Compute VOC AP given precision and recall.
+    If use_07_metric is true, uses the
+    VOC 07 11 point method (default:False).
+    """
+    if use_07_metric:  #VOC在2010之后换了评价方法，所以决定是否用07年的
+        # 11 point metric
+        ap = 0.
+        for t in np.arange(0., 1.1, 0.1):  #  07年的采用11个点平分recall来计算
+            if np.sum(rec >= t) == 0:
+                p = 0
+            else:
+                p = np.max(prec[rec >= t])  # 取一个recall阈值之后最大的precision
+            ap = ap + p / 11.  # 将11个precision加和平均
+    else:  # 这里是用2010年后的方法，取所有不同的recall对应的点处的精度值做平均，不再是固定的11个点
+        # correct AP calculation
+        # first append sentinel values at the end
+        mrec = np.concatenate(([0.], rec, [1.]))  #recall和precision前后分别加了一个值，因为recall最后是1，所以
+        mpre = np.concatenate(([0.], prec, [0.])) # 右边加了1，precision加的是0
 
-def FP_TP_batch(preds_bbxs, preds_labels, truth_bbxs, truth_labels, anchors, decode_box, nums_class, iou_threshold=0.5, in_fmt='cxcywh', device='cuda:0'):
+        # compute the precision envelope
+        for i in range(mpre.size - 1, 0, -1):
+            mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])  #从后往前，排除之前局部增加的precison情况
+
+        # to calculate area under PR curve, look for points
+        # where X axis (recall) changes value
+        i = np.where(mrec[1:] != mrec[:-1])[0]  # 这里巧妙的错位，返回刚好TP的位置，
+                                                                                      # 可以看后面辅助的例子
+
+        # and sum (\Delta recall) * prec   用recall的间隔对精度作加权平均
+        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+def FP_TP_batch(preds_bbxs, preds_labels, truth_bbxs, truth_labels, anchors, decode_box, num_classes, iou_threshold=0.5, in_fmt='cxcywh', device='cuda:0'):
+
+    #preds_bbxs: [B x num_anchors x 4] decode cx cy w h
+    #preds_labels: [B x num_anchors x num_class]
+    #truth_bbxs: [[num_truth_box(i) x 4] for i in B] cx cy w h
+    #truth_labels: [[num_truth_box(i)] for i in B]
+
     assert preds_bbxs.shape[0] == len(truth_bbxs)
     anchors = anchors.to(device)
     out_bbxs = []
     preds_map = []
-    prop_clss, pred_clss = preds_labels.max(dim=2)
+    prop_clss, pred_clss = preds_labels.max(dim=-1) # B x num_anchors
     pos_pred_clss = pred_clss != 0
-    for i in range(preds_bbxs.shape[0]):
-        converted_bx = box_convert(decode_box(
-            anchors, preds_bbxs[i]), in_fmt, 'xyxy').clamp(0, 1)
-        if torch.any(pos_pred_clss[i]):
-            # cleaned background preds
-            after_nms = nms(
-                converted_bx[pos_pred_clss[i]], prop_clss[i][pos_pred_clss[i]], 0.5)
-            out_bbxs.append(converted_bx[pos_pred_clss[i]][after_nms, :])
-            preds_map.append(preds_labels[i][pos_pred_clss[i]][after_nms, :])
-        else:
-            out_bbxs.append(converted_bx)
-            preds_map.append(preds_labels[i])
 
-    dglob = {}
-    gt_glob = {}
-    for i in range(1, nums_class):
-        dglob[i] = []
-        gt_glob[i] = 0
-    for i in range(len(truth_bbxs)):
-        truth_bbxs_cvt = box_convert(
-            truth_bbxs[i], in_fmt, 'xyxy').clamp(0, 1).to(device)
-        d, gt_d = FP_TP_one_img(
-            out_bbxs[i], preds_map[i], truth_bbxs_cvt, truth_labels[i], nums_class, iou_threshold, device=device)
-        for k in d.keys():
-            if len(d[k]) > 0:
-                dglob[k].append(d[k])
-            gt_glob[k] += gt_d[k]
-    for i in range(1, nums_class):
-        if len(dglob[i]) == 0:
+    #for each predicted batch
+    for i in range(preds_bbxs.shape[0]):
+        #convert the predicted boxes to true coordinates (xyxy)
+        converted_bx = box_convert(decode_box(preds_bbxs[i],anchors
+             ), in_fmt, 'xyxy').clamp_(0, 1)
+        #if there is any predicted class is positive (eg. not back ground)
+        #if torch.any(pos_pred_clss[i]):
+            # cleaned background preds
+        after_nms = nms(
+            converted_bx[pos_pred_clss[i]], prop_clss[i][pos_pred_clss[i]], 0.6)
+        out_bbxs.append([converted_bx[pos_pred_clss[i]][after_nms, :]])
+        preds_map.append([prop_clss[i][pos_pred_clss[i]][after_nms],pred_clss[i][pos_pred_clss[i]][after_nms]])
+
+    #out_bbxs as well as preds_map has the same len as truth_bbxs
+    average_precisions = []
+
+    # used for numerical stability later on
+    epsilon = 1e-6
+
+    for c in range(1,num_classes):
+        detections = []
+        ground_truths = []
+
+        # Go through all predictions and targets,
+        # and only add the ones that belong to the
+        # current class c
+        # for (detection,predict_label) in zip(out_bbxs,pred_clss):
+        #     if predict_label == c:
+        #         detections.append(detection)
+        for i, (cur_img_pred_bbxs, cur_img_pred_label) in enumerate(list(zip(out_bbxs,preds_map))):
+            for idx,cur_pred_bbxs in enumerate(cur_img_pred_bbxs):
+                if cur_pred_bbxs.shape[0] and cur_img_pred_label[1][idx]==c:
+                    detections.append([i,cur_img_pred_label[0][idx],cur_img_pred_label[1][idx],cur_pred_bbxs])
+
+
+
+        # for (true_box,true_label) in zip(truth_bbxs,truth_labels):
+        #     if true_label == c:
+        #         ground_truths.append(true_box)
+
+        for i, (cur_img_true_bbxs, cur_img_true_label) in enumerate(list(zip(truth_bbxs,truth_labels))):
+            for cur_true_bbxs,cur_true_label in zip(cur_img_true_bbxs,cur_img_true_label):
+                if cur_true_label==c:
+                    ground_truths.append([i,cur_true_label,box_convert(cur_true_bbxs,in_fmt=in_fmt,out_fmt='xyxy').clamp_(0, 1)])
+
+        # find the amount of bboxes for each training example
+        # Counter here finds how many ground truth bboxes we get
+        # for each training example, so let's say img 0 has 3,
+        # img 1 has 5 then we will obtain a dictionary with:
+        # amount_bboxes = {0:3, 1:5}
+        amount_bboxes = Counter([gt[0] for gt in ground_truths])
+
+        # We then go through each key, val in this dictionary
+        # and convert to the following (w.r.t same example):
+        # ammount_bboxes = {0:torch.tensor[0,0,0], 1:torch.tensor[0,0,0,0,0]}
+        for key, val in amount_bboxes.items():
+            amount_bboxes[key] = torch.zeros(val)
+
+        # sort by box probabilities which is index 2
+        detections.sort(key=lambda x: x[2], reverse=True)
+        TP = torch.zeros((len(detections)))
+        FP = torch.zeros((len(detections)))
+        total_true_bboxes = len(ground_truths)
+        
+        # If none exists for this class then we can safely skip
+        if total_true_bboxes == 0:
             continue
-        dglob[i] = torch.cat(dglob[i])
-    return dglob, gt_glob
+
+        for detection_idx, detection in enumerate(detections):
+            # Only take out the ground_truths that have the same
+            # training idx as detection
+            ground_truth_img = [
+                bbox for bbox in ground_truths if bbox[0] == detection[0]
+            ]
+
+            num_gts = len(ground_truth_img)
+            best_iou = 0
+
+            for idx, gt in enumerate(ground_truth_img):
+                # tmp1 = detection[3].unsqueeze(0)
+                # tmp2 = gt[2].unsqueeze(0)
+                iou = box_iou(
+                    detection[3].to(device=device),
+                    gt[2].unsqueeze(0).to(device=device),
+                )[0,0]
+
+                if iou > best_iou:
+                    best_iou = iou
+                    best_gt_idx = idx
+
+            if best_iou > iou_threshold:
+                # only detect ground truth detection once
+                if amount_bboxes[detection[0]][best_gt_idx] == 0:
+                    # true positive and add this bounding box to seen
+                    TP[detection_idx] = 1
+                    amount_bboxes[detection[0]][best_gt_idx] = 1
+                else:
+                    FP[detection_idx] = 1
+
+            # if IOU is lower then the detection is a false positive
+            else:
+                FP[detection_idx] = 1
+
+        TP_cumsum = torch.cumsum(TP, dim=0)
+        FP_cumsum = torch.cumsum(FP, dim=0)
+        recalls = TP_cumsum / (total_true_bboxes + epsilon)
+        precisions = TP_cumsum / (TP_cumsum + FP_cumsum + epsilon)
+        precisions = torch.cat((torch.tensor([1]), precisions))
+        recalls = torch.cat((torch.tensor([0]), recalls))
+        # torch.trapz for numerical integration
+        average_precisions.append(voc_ap(recalls,precisions))
+
+    return sum(average_precisions) / len(average_precisions)
+
+    
+
+
+        
 
 
 def mAP(dglob, gt_glob):
@@ -118,12 +214,13 @@ def mAP(dglob, gt_glob):
 
 
 def wrapper_mAP(preds_bbxs, preds_labels, truth_bbxs, truth_labels, anchors, decode_box, nums_class, iou_threshold=0.5, in_fmt='cxcywh'):
-    dglob, gt_glob = FP_TP_batch(preds_bbxs, preds_labels, truth_bbxs, truth_labels,
+    return FP_TP_batch(preds_bbxs, preds_labels, truth_bbxs, truth_labels,
                                  anchors, decode_box, nums_class, iou_threshold=iou_threshold, in_fmt=in_fmt)
-    return mAP(dglob, gt_glob)
+    # return mAP(dglob, gt_glob)
 
 
 if __name__ == "__main__":
     a = torch.tensor([[1, 2], [3, 4]])
     print(a)
     print(a[[False, True]])
+

main.py
@@ -7,47 +7,54 @@ from config.config import ROOT, dataset_configs
 from data.box_utils import *
 import pandas as pd
 from functools import partial
+import torch.optim as optim
 
 if __name__ == "__main__":
     df_raw = pd.read_csv(dataset_configs['annotations'])
     df_train, df_valid, df_test = split_dataset(df_raw, 10)
     my_model = SSDLite(num_class=3).cuda()
     optimizer = optim.SGD(my_model.parameters(),
-                          lr=0.1333333, momentum=0.9)
+                          lr=0.00002333333,momentum=0.9)
+    my_model.load_state_dict(torch.load('test4.pth'))
+    my_model.feature_extractor.unfreeze_base()
     n_epochs = 100
     train_ds = ImageData(
         df_train, df_raw, dataset_configs['imgs_path'], phase='train')
     val_ds = ImageData(
         df_valid, df_raw, dataset_configs['imgs_path'], phase='valid')
-    train_loader = DataLoader(train_ds, 16, num_workers=4,
+    train_loader = DataLoader(train_ds, 32, num_workers=6,
                               pin_memory=True, collate_fn=train_ds.collate_fn, shuffle=True)
     valid_loader = DataLoader(val_ds, 16, num_workers=4,
                               pin_memory=True, collate_fn=val_ds.collate_fn, shuffle=False)
     default_anchors = AnchorBox().get_anchor()
     wrapper_mAP1 = partial(wrapper_mAP,
-                           decode_box=decode_coordinate, nums_class=3, anchors=default_anchors)
+                           decode_box=decode_new, nums_class=3, anchors=default_anchors)
     # train(my_model, optimizer, default_anchors, train_loader=train_loader,
     #       valid_loader=valid_loader, n_epochs=n_epochs,
     #       bx_match_func=match_batch, loss=multibox_loss, evaluate_metric=wrapper_mAP1,
-    #       save_path='./test3.pth', device='cuda:0', save_plot_path='.test_img/train_plot.png')
+    #       save_path='./test_fix_mAP.pth', device='cuda:0', save_plot_path='.test_img/train_plot.png')
     # -------------------------------------------------test inference---------------------------------------------------
-    my_model.load_state_dict(torch.load('test2.pth'))
+    my_model.load_state_dict(torch.load('test_fix_mAP.pth'))
     my_model = my_model.eval().cuda()
-    test_ds = ImageData(
-        df_test, df_raw, dataset_configs['imgs_path'], phase='test')
-    img_tensor, a, b, img_path = test_ds[2]
+    test_ds = ImageData(df_test, df_raw, dataset_configs['imgs_path'], phase='test')
+    test_dl = DataLoader(test_ds, 16, num_workers=4, collate_fn=test_ds.collate_fn, shuffle=False)
+    print(test_model(my_model,default_anchors,'cuda:0',test_dl,match_batch,wrapper_mAP1,loss=multibox_loss))
+
+
+
+    img_tensor, a, b, img_path = test_ds[1111]
     img_tensor = img_tensor[None]
     out = my_model(img_tensor.cuda())
     _deltas, _clss = out['regression'], out['classification'].softmax(dim=-1)
     default_anchors = default_anchors.cuda()
-    coord = decode_coordinate(default_anchors, _deltas.squeeze())
+    coord = decode_new(_deltas.squeeze(),default_anchors,)
     def_box = torchvision.ops.box_convert(
-        coord, in_fmt='cxcywh', out_fmt='xyxy')  # def * numclss
+        coord, in_fmt='cxcywh', out_fmt='xyxy').clamp_(0,1)  # def * numclss
     conf, idxs = _clss.squeeze().max(dim=-1)
     pos = idxs != 0
     conf_pos = conf[pos]
     def_pos = def_box[pos]
-    idx = nms(def_pos, conf_pos, 0.3)
+    idx = nms(def_pos, conf_pos, 0.6)
     h, w, _ = np.array(Image.open(img_path)).shape
     fin = def_pos[idx].clamp_(min=0, max=1)*torch.tensor([w, h, w, h]).cuda()
     k = 5
@@ -55,3 +62,6 @@ if __name__ == "__main__":
             for i, j in zip(idxs[pos][idx], conf_pos[idx])]
     show(Image.open(img_path), bbs=fin, texts=text)
     plt.savefig('test_img/test_predict.png')
+
+
+

models/ssd.py
@@ -4,15 +4,16 @@ from models.mobilenet import mobilenetV3_Lite, baseModel, simple_conv
 from torchvision.models import mobilenet_v3_small  # for load pretrain
 import torch.nn as nn
 import torch
+import functools
 
 
 def extra_block(in_channels, out_channels, is_BN):
     activation = nn.ReLU6
     inter_channels = out_channels//2
-    extras = nn.Sequential(simple_conv(in_channels, inter_channels, 1, 1, activation=activation, isBN=is_BN),
+    extras = nn.Sequential(simple_conv(in_channels, inter_channels, 1, 1, activation=activation, isBN=is_BN,bias=not is_BN),
                            simple_conv(inter_channels, inter_channels, 3, 3,
-                                       activation=activation, groups=inter_channels, stride=2, padding=1, isBN=is_BN),
-                           simple_conv(inter_channels, out_channels, 1, 1, activation=activation, isBN=is_BN),)
+                                       activation=activation, groups=inter_channels, stride=2, padding=1, isBN=is_BN,bias=not is_BN),
+                           simple_conv(inter_channels, out_channels, 1, 1, activation=activation, isBN=is_BN,bias=not is_BN),)
     return extras
 
 
@@ -23,13 +24,18 @@ def normal_init(conv: nn.Module):
             if layer.bias is not None:
                 torch.nn.init.constant_(layer.bias, 0.0)
 
+def xavier_init_(m: nn.Module):
+    if isinstance(m, nn.Conv2d):
+        nn.init.xavier_uniform_(m.weight)
 
 def prediction_block(in_channels, out_channels, kernel_size, is_BN):
     activation = nn.ReLU6
     padding = (kernel_size - 1) // 2
     return nn.Sequential(simple_conv(in_channels, in_channels, kernel_size,
-                         kernel_size, groups=in_channels, isBN=is_BN, padding=padding, activation=activation),
-                         simple_conv(in_channels, out_channels, 1, 1, isBN=False, activation=None))
+                         kernel_size, groups=in_channels, isBN=is_BN,bias=not is_BN, padding=padding, activation=activation),
+                         simple_conv(in_channels, in_channels, kernel_size,
+                         kernel_size, groups=in_channels, isBN=is_BN,bias=not is_BN, padding=padding, activation=activation),
+                         simple_conv(in_channels, out_channels, 1, 1, isBN=False, activation=None,bias=True))
 
 
 class SSDLiteClassificationHead(baseModel):
@@ -48,9 +54,8 @@ class SSDLiteClassificationHead(baseModel):
         for x, block in zip(xs, self.class_heads):
             temp = block(x)
             # B x num_classes x H x W
-            ret.append(
-                temp.view(temp.shape[0], self.num_classes, -1).contiguous().permute(0, 2, 1))
-        return torch.cat(ret, dim=1)  # B x num_anchors x num_classes
+            ret.append(temp.permute(0, 2, 3, 1).contiguous())
+        return torch.cat([o.view(o.size(0), -1) for o in ret], 1) # B x num_anchors x num_classes
 
 
 class SSDLiteRegressorHead(baseModel):
@@ -67,9 +72,8 @@ class SSDLiteRegressorHead(baseModel):
         ret = []
         for x, block in zip(xs, self.reg_heads):
             temp = block(x)
-            ret.append(temp.view(
-                temp.shape[0], 4, -1).contiguous().permute(0, 2, 1))  # B x 4 x (H x W x num_anchors_per_cell)
-        return torch.cat(ret, dim=1)  # B x num_anchors x 4
+            ret.append(temp.permute(0, 2, 3, 1).contiguous())  # B x 4 x (H x W x num_anchors_per_cell)
+        return torch.cat([o.view(o.size(0), -1) for o in ret], 1) # B x num_anchors x 4
 
 
 class SSDLiteHead(baseModel):
@@ -79,11 +83,14 @@ class SSDLiteHead(baseModel):
             in_channels, num_anchors, num_class, is_BN)
         self.regressor_head = SSDLiteRegressorHead(
             in_channels, num_anchors, is_BN)
-        normal_init(self)
+        self.num_classes = num_class
+        xavier_init_(self)
 
     def forward(self, x):
         # x: list of tensors of shape B x C x H x W
-        return {"classification": self.classification_head(x), "regression": self.regressor_head(x)}
+        loc = self.regressor_head(x)
+        conf = self.classification_head(x)
+        return {"classification": conf.view(conf.size(0), -1, self.num_classes), "regression":   loc.view(loc.size(0), -1, 4)}
 
 
 class Mobilenetv3SmallExtra(baseModel):
@@ -103,7 +110,7 @@ class Mobilenetv3SmallExtra(baseModel):
             extra_block(256, 128, is_BN)
         ]
         self.extras = nn.ModuleList(self.extras)
-        normal_init(self.extras)
+        xavier_init_(self.extras)
 
     def forward(self, x):
         # x: BxCxHxW

scripts/train_one_batch.py
@@ -1,11 +1,18 @@
+import torch
+scalar = torch.cuda.amp.GradScaler()
 def train_batch(model, loss, data, optimizer):
-    optimizer.zero_grad()
+
     imgs, target = data
+   # with torch.cuda.amp.autocast():
     preds = model(imgs)
     preds_bbxs, preds_labels = preds['regression'], preds['classification']
-    target_bbxs, target_labels = target['bbxs'], target['clss']
+    target_bbxs, target_labels,priors = target['bbxs'], target['clss'],target['anchor']
     cur_loss, box_loss, class_loss = loss(
-        preds_labels, preds_bbxs, target_labels,  target_bbxs)
+        preds_labels, preds_bbxs, target_labels,  target_bbxs,priors)
+# scalar.scale(cur_loss).backward()
+    # scalar.step(optimizer)
+    # scalar.update()
+    optimizer.zero_grad()
     cur_loss.backward()
     optimizer.step()
     return cur_loss.cpu().detach().item(), box_loss.detach().cpu().item(), class_loss.detach().cpu().item()
@@ -16,9 +23,9 @@ def valid_batch(model, loss, data, evaluate_metric):
     imgs, target, truth_bbxs, truth_labels = data
     preds = model(imgs)
     preds_bbxs, preds_labels = preds['regression'], preds['classification']
-    target_bbxs, target_labels = target['bbxs'], target['clss']
+    target_bbxs, target_labels,priors = target['bbxs'], target['clss'],target['anchor']
     cur_loss, box_loss, class_loss = loss(
-        preds_labels, preds_bbxs, target_labels,  target_bbxs)
+        preds_labels, preds_bbxs, target_labels,  target_bbxs,priors)
     eval_val = evaluate_metric(
         preds_bbxs, preds_labels, truth_bbxs, truth_labels)
     return cur_loss.cpu().detach().item(), box_loss.detach().cpu().item(), class_loss.detach().cpu().item(), eval_val